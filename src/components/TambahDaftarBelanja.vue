6f7e7be92b61fdb1dc5860b5b12ec2bb58cf008e
Muhammad Rydwan <muhammadrydwanmylike10@gmail.com>
2020-05-05 22:11:27 +0700

    Menambahkan lagi validasi form. Besok fix validasi per form dan fix kolom dan row

--- src/components/TambahDaftarBelanja.vue
+++ src/components/TambahDaftarBelanja.vue
@@ -19,21 +19,23 @@
           </div>
           <div class="form-group">
             <label for="satuan">Satuan</label>
-            <select name="satuan" class="form-control" v-model="satuan">
-              <option value selected disabled>Pilih Satuan</option>
+            <select name="satuan" class="form-control" v-bind:class="{'is-invalid': validasi}" v-model="satuan">
+              <option value='' selected disabled>Pilih Satuan</option>
               <option value="pcs">Pcs</option>
               <option value="kg">Kg</option>
               <option value="liter">Liter</option>
               <option value="renteng">Renteng</option>
               <option value="karton">Karton</option>
             </select>
+            <small v-if="validasi" class="form-text text-danger">Satuan harus dipilih!</small>
           </div>
           <label for="hargaSatuan">Harga Satuan</label>
           <div class="input-group mb-2 mr-sm-2">
             <div class="input-group-prepend">
               <div class="input-group-text">Rp.</div>
             </div>
-            <input type="text" class="form-control" name="hargaSatuan" v-model="hargaSatuan" />
+            <input type="number" class="form-control" v-bind:class="{'is-invalid': validasi}" name="hargaSatuan" v-model="hargaSatuan" />
+            <small v-if="validasi" class="form-text text-danger">Harga satuan harus diisi dan tidak boleh negatif!</small>
           </div>
 
           <button class="btn btn-success btn-block mt-4" type="submit">Proses</button>
@@ -97,6 +99,13 @@ export default {
       if(this.jumlah <= 0){
         return this.validasi = true;
       }else if(this.jumlah != ''){
+        return this.validasi = false;
+      }
+    },
+    hargaSatuan: function(){
+      if(this.hargaSatuan <= 0){
+        return this.validasi = true;
+      }else if(this.hargaSatuan != ''){
         return this.validasi = false;
       }
     }
